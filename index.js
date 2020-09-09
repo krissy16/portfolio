8e68906280ee5cc0cd66c58d194154da76414123
Krissy <krissysaenz@gmail.com>
2020-09-09 13:50:02 -0700

    added form listener

--- index.js
+++ index.js
@@ -140,9 +140,17 @@ function addIcons(index){
     return icons.join(' ');
 }
 
+function handleForm(){
+    let form = document.getElementById('form')
+    form.addEventListener('submit', () => {
+        window.top.location = 'mailto:...';
+    })
+}
+
 function uponStart(){
     addProjects();
     handleNav();
+    handleForm();
 }
 
 $(uponStart);
